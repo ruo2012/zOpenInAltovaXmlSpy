480a46bc138bf32f8e19e02334ad14a0ade9042f
Gmaniacuk1 <gtrevellick@googlemail.com>
2016-12-28 19:21:38 +0000

    added new class

---
+++ OpenInApp.Common/Helpers/CommonFileHelper.cs
@@ -0,0 +1,18 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using OpenInApp.Common.Dtos;
+using OpenInApp.Common.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace OpenInApp.Common.Helpers
+{
+    public class CommonFileHelper
+    {
+    }
+}
