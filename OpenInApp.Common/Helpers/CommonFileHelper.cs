385bc1d7d1206eb4262a6fc288352789f339764b
Gmaniacuk1 <gtrevellick@googlemail.com>
2016-12-29 10:58:06 +0000

    comments

--- OpenInApp.Common/Helpers/CommonFileHelper.cs
+++ OpenInApp.Common/Helpers/CommonFileHelper.cs
@@ -1,15 +1,12 @@
 ﻿using EnvDTE;
 using EnvDTE80;
 using OpenInApp.Common.Dtos;
-//using OpenInApp.Common.Dtos;
-//using OpenInApp.Common.Helpers;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-//using System.Threading.Tasks;
 
 namespace OpenInApp.Common.Helpers
 {
